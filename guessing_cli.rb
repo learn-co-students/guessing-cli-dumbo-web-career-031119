3c1bf62e3cb60d7255e7f94ddc35c92b9b610d19
Jerry <jvr572@Gmail.com>
2019-03-10 17:25:43 -0400

    Done.

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -9,6 +9,7 @@ def run_guessing_game
       puts "You guessed the correct number!"
     when "exit"
       puts "Goodbye!"
+      break
     else
       puts "The computer guessed #{ran_num}."
     end
